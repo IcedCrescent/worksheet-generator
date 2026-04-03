86f49a0f8c314fc157363010389fcf85bef07f1e
IcedCrescent <daoquangtrung1998@outlook.com.vn>
2026-04-03 15:35:43 +0700

    Fix wording on the canvas

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -1,31 +1,31 @@
 <script lang="ts">
-  import { dndzone, type DndEvent } from 'svelte-dnd-action';
-  import { flip } from 'svelte/animate';
-  import { worksheetBlocks, type WorksheetBlock } from '../lib/store';
-  
-  import SequenceAnalysis from './problems/SequenceAnalysis.svelte';
-  import NumberToWords from './problems/NumberToWords.svelte';
-  import SequenceWord from './problems/SequenceWord.svelte';
-  import Multiplication from './problems/Multiplication.svelte';
-  import TrueFalseStatements from './problems/TrueFalseStatements.svelte';
-  import NumberLineJumps from './problems/NumberLineJumps.svelte';
-  import TimeDuration from './problems/TimeDuration.svelte';
-  import DecomposeNumber from './problems/DecomposeNumber.svelte';
-  import ProbabilityBag from './problems/ProbabilityBag.svelte';
-  import AnalogClockProblem from './problems/AnalogClockProblem.svelte';
-  import TimeTable from './problems/TimeTable.svelte';
-  import TimeMatching from './problems/TimeMatching.svelte';
-  import TimeDifference from './problems/TimeDifference.svelte';
-  import TimeConversion from './problems/TimeConversion.svelte';
-  import NumberComposition from './problems/NumberComposition.svelte';
-  import MultiplesFactors from './problems/MultiplesFactors.svelte';
-  import OrderNumberLine from './problems/OrderNumberLine.svelte';
-  import SequenceClaim from './problems/SequenceClaim.svelte';
-  import PlaceValueDecode from './problems/PlaceValueDecode.svelte';
-  import MathFlowDiagram from './problems/MathFlowDiagram.svelte';
-  import NumberCardMatch from './problems/NumberCardMatch.svelte';
-  import FindIncorrectDecomposition from './problems/FindIncorrectDecomposition.svelte';
-  import NumberCardCalc from './problems/NumberCardCalc.svelte';
+  import { dndzone, type DndEvent } from "svelte-dnd-action";
+  import { flip } from "svelte/animate";
+  import { worksheetBlocks, type WorksheetBlock } from "../lib/store";
+
+  import SequenceAnalysis from "./problems/SequenceAnalysis.svelte";
+  import NumberToWords from "./problems/NumberToWords.svelte";
+  import SequenceWord from "./problems/SequenceWord.svelte";
+  import Multiplication from "./problems/Multiplication.svelte";
+  import TrueFalseStatements from "./problems/TrueFalseStatements.svelte";
+  import NumberLineJumps from "./problems/NumberLineJumps.svelte";
+  import TimeDuration from "./problems/TimeDuration.svelte";
+  import DecomposeNumber from "./problems/DecomposeNumber.svelte";
+  import ProbabilityBag from "./problems/ProbabilityBag.svelte";
+  import AnalogClockProblem from "./problems/AnalogClockProblem.svelte";
+  import TimeTable from "./problems/TimeTable.svelte";
+  import TimeMatching from "./problems/TimeMatching.svelte";
+  import TimeDifference from "./problems/TimeDifference.svelte";
+  import TimeConversion from "./problems/TimeConversion.svelte";
+  import NumberComposition from "./problems/NumberComposition.svelte";
+  import MultiplesFactors from "./problems/MultiplesFactors.svelte";
+  import OrderNumberLine from "./problems/OrderNumberLine.svelte";
+  import SequenceClaim from "./problems/SequenceClaim.svelte";
+  import PlaceValueDecode from "./problems/PlaceValueDecode.svelte";
+  import MathFlowDiagram from "./problems/MathFlowDiagram.svelte";
+  import NumberCardMatch from "./problems/NumberCardMatch.svelte";
+  import FindIncorrectDecomposition from "./problems/FindIncorrectDecomposition.svelte";
+  import NumberCardCalc from "./problems/NumberCardCalc.svelte";
 
   const flipDurationMs = 200;
 
@@ -38,12 +38,11 @@
   }
 
   function removeBlock(id: string) {
-    worksheetBlocks.update(blocks => blocks.filter(b => b.id !== id));
+    worksheetBlocks.update((blocks) => blocks.filter((b) => b.id !== id));
   }
 </script>
 
 <div class="worksheet-page">
-  
   <div class="worksheet-header">
     <div class="header-row">
       <div class="field">Name: <span class="dotted-line"></span></div>
@@ -55,83 +54,90 @@
     </div>
   </div>
 
-  <section 
-    class="dnd-zone" 
-    use:dndzone={{items: $worksheetBlocks, flipDurationMs, dropTargetStyle: {outline: '2px dashed var(--accent)'}}} 
-    on:consider={handleDndConsider} 
+  <section
+    class="dnd-zone"
+    use:dndzone={{
+      items: $worksheetBlocks,
+      flipDurationMs,
+      dropTargetStyle: { outline: "2px dashed var(--accent)" },
+    }}
+    on:consider={handleDndConsider}
     on:finalize={handleDndFinalize}
   >
     {#each $worksheetBlocks as block, index (block.id)}
-      <div class="problem-wrapper problem-block" animate:flip={{duration: flipDurationMs}}>
-        
+      <div
+        class="problem-wrapper problem-block"
+        animate:flip={{ duration: flipDurationMs }}
+      >
         <div class="block-controls problem-block-controls no-print">
           <span class="drag-handle">✋ Drag to move</span>
-          <button class="remove-btn" on:click={() => removeBlock(block.id)}>✕</button>
+          <button class="remove-btn" on:click={() => removeBlock(block.id)}
+            >✕</button
+          >
         </div>
 
         <div class="problem-content">
           <div class="problem-number">{index + 1}.</div>
           <div class="problem-body">
-            {#if block.type === 'sequence-analysis'}
+            {#if block.type === "sequence-analysis"}
               <SequenceAnalysis bind:data={block.data} />
-            {:else if block.type === 'number-to-words'}
+            {:else if block.type === "number-to-words"}
               <NumberToWords bind:data={block.data} />
-            {:else if block.type === 'sequence-word'}
+            {:else if block.type === "sequence-word"}
               <SequenceWord bind:data={block.data} />
-            {:else if block.type === 'multiplication'}
+            {:else if block.type === "multiplication"}
               <Multiplication bind:data={block.data} />
-            {:else if block.type === 'true-false'}
+            {:else if block.type === "true-false"}
               <TrueFalseStatements bind:data={block.data} />
-            {:else if block.type === 'number-line-jumps'}
+            {:else if block.type === "number-line-jumps"}
               <NumberLineJumps bind:data={block.data} />
-            {:else if block.type === 'time-duration'}
+            {:else if block.type === "time-duration"}
               <TimeDuration bind:data={block.data} />
-            {:else if block.type === 'decompose-number'}
+            {:else if block.type === "decompose-number"}
               <DecomposeNumber bind:data={block.data} />
-            {:else if block.type === 'probability-bag'}
+            {:else if block.type === "probability-bag"}
               <ProbabilityBag bind:data={block.data} />
-            {:else if block.type === 'analog-clock'}
+            {:else if block.type === "analog-clock"}
               <AnalogClockProblem bind:data={block.data} />
-            {:else if block.type === 'time-table'}
+            {:else if block.type === "time-table"}
               <TimeTable bind:data={block.data} />
-            {:else if block.type === 'time-matching'}
+            {:else if block.type === "time-matching"}
               <TimeMatching bind:data={block.data} />
-            {:else if block.type === 'time-difference'}
+            {:else if block.type === "time-difference"}
               <TimeDifference bind:data={block.data} />
-            {:else if block.type === 'time-conversion'}
+            {:else if block.type === "time-conversion"}
               <TimeConversion bind:data={block.data} />
-            {:else if block.type === 'number-composition'}
+            {:else if block.type === "number-composition"}
               <NumberComposition bind:data={block.data} />
-            {:else if block.type === 'multiples-factors'}
+            {:else if block.type === "multiples-factors"}
               <MultiplesFactors bind:data={block.data} />
-            {:else if block.type === 'order-number-line'}
+            {:else if block.type === "order-number-line"}
               <OrderNumberLine bind:data={block.data} />
-            {:else if block.type === 'sequence-claim'}
+            {:else if block.type === "sequence-claim"}
               <SequenceClaim bind:data={block.data} />
-            {:else if block.type === 'place-value-decode'}
+            {:else if block.type === "place-value-decode"}
               <PlaceValueDecode bind:data={block.data} />
-            {:else if block.type === 'math-flow-diagram'}
+            {:else if block.type === "math-flow-diagram"}
               <MathFlowDiagram bind:data={block.data} />
-            {:else if block.type === 'number-card-match'}
+            {:else if block.type === "number-card-match"}
               <NumberCardMatch bind:data={block.data} />
-            {:else if block.type === 'find-incorrect-decomp'}
+            {:else if block.type === "find-incorrect-decomp"}
               <FindIncorrectDecomposition bind:data={block.data} />
-            {:else if block.type === 'number-card-calc'}
+            {:else if block.type === "number-card-calc"}
               <NumberCardCalc bind:data={block.data} />
             {/if}
           </div>
         </div>
       </div>
     {/each}
-    
+
     {#if $worksheetBlocks.length === 0}
       <div class="empty-state no-print">
         <p>Your worksheet is empty.</p>
-        <p>Drag or click problems from the sidebar to add them.</p>
+        <p>Click on problems from the sidebar to add them.</p>
       </div>
     {/if}
   </section>
-
 </div>
 
 <style>
